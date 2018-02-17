7206c13efdeba2d9056ee379c52bd768b8f20cfb
Bobby White <bobby.white@salesforce.com>
2018-02-16 19:34:28 -0500

    configured for my DevHub org

---
+++ env.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+export CONSUMER_KEY=3MVG9CEn_O3jvv0ytCSYeY_Be53MeBMzVpDHN.GSiIpZP5vjcmNLgP86narE8VPhkJbB_.IHGLeLxK2dppEZj
+export JWT_KEY_FILE=/Users/bobby.white/projects/certificates/server.key
+export HUB_USERNAME=bobby.white@salesforce.com.devhub
