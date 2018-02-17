5ebcf77fc2b82c198edb7e656546dd984355c15e
Bobby White <bobby.white@salesforce.com>
2018-02-16 19:43:33 -0500

    meaningless update

--- env.sh
+++ env.sh
@@ -2,3 +2,4 @@
 export CONSUMER_KEY=3MVG9CEn_O3jvv0ytCSYeY_Be53MeBMzVpDHN.GSiIpZP5vjcmNLgP86narE8VPhkJbB_.IHGLeLxK2dppEZj
 export JWT_KEY_FILE=/Users/bobby.white/projects/certificates/server.key
 export HUB_USERNAME=bobby.white@salesforce.com.devhub
+# Source this file to set these environment variables
